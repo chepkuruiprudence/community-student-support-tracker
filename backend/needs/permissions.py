77ea5aae8078703f6c279f5a690496a956430a5f
chepkuruiprudence <chepkuruiprudence403@gmail.com>
2025-12-17 01:57:29 -0800

    Fix custom user model references and reset migrations

---
+++ backend/needs/permissions.py
@@ -0,0 +1,10 @@
+from rest_framework.permissions import BasePermission
+
+class IsStudent(BasePermission):
+    def has_permission(self, request, view):
+        return request.user.is_authenticated and request.user.is_student
+
+
+class IsOwnerStudent(BasePermission):
+    def has_object_permission(self, request, view, obj):
+        return request.user.is_authenticated and obj.student == request.user
--- backend/needs/views.py
+++ backend/needs/views.py
@@ -1,12 +1,56 @@
 from rest_framework.viewsets import ModelViewSet
+from rest_framework.permissions import IsAuthenticated
+from rest_framework.decorators import action
+from rest_framework.response import Response
+from rest_framework import status
+
 from .models import Need
-from needs.serializers import NeedSerializer
+from .serializers import NeedSerializer
+from .permissions import IsStudent, IsDonor
+
 
 class NeedViewSet(ModelViewSet):
     serializer_class = NeedSerializer
+    permission_classes = [IsAuthenticated, IsStudent]
 
     def get_queryset(self):
-        return Need.objects.filter(user=self.request.user)
+        # Students only see THEIR needs
+        return Need.objects.filter(student=self.request.user)
 
     def perform_create(self, serializer):
-        serializer.save(user=self.request.user)
+        serializer.save(student=self.request.user)
+
+    @action(
+        detail=True,
+        methods=['post'],
+        permission_classes=[IsAuthenticated, IsDonor]
+    )
+    def pledge(self, request, pk=None):
+        need = self.get_object()
+        amount = request.data.get("amount")
+
+        if not amount:
+            return Response(
+                {"error": "Amount is required"},
+                status=status.HTTP_400_BAD_REQUEST
+            )
+
+        try:
+            amount = float(amount)
+        except ValueError:
+            return Response(
+                {"error": "Invalid amount"},
+                status=status.HTTP_400_BAD_REQUEST
+            )
+
+        need.amount_pledged += amount
+
+        if need.amount_pledged >= need.amount_required:
+            need.status = "funded"
+
+        need.save()
+
+        return Response(
+            {"message": "Pledge successful"},
+            status=status.HTTP_200_OK
+        )
